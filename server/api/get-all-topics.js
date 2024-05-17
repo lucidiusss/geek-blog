0a2748e59d3b7ed050147b9ec8b3ddba8a284b8a
lucidiusss <cheapp78@gmail.com>
2024-05-17 22:19:44 +0300

    .

--- server/api/get-all-topics.js
+++ server/api/get-all-topics.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async () => {
-  let topics = await prisma.categories.findMany({
+  let topics = await prisma.category.findMany({
     orderBy: { id: "desc" },
   });
   return topics;
